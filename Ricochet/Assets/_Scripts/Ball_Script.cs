80b3b28f7e0d889dd7c2d21c6ac8853085104a3a
austen <u0941256@utah.edu>
2017-09-17 17:49:12 -0600

    white box level filled out ball script score UI implimented

---
+++ Ricochet/Assets/_Scripts/Ball_Script.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Script : MonoBehaviour {
+    public Vector2 initialForce = new Vector2(255, 157);
+    public float resetDelay = 2f;
+
+    private Rigidbody2D body;
+    private Vector2 start;
+    private SpriteRenderer sprite;
+    // should I be using a lock?
+    private bool hidden;
+    // Use this for initialization
+    void Start() {
+        body = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+
+        start = transform.position;
+
+        body.AddForce(initialForce);
+    }
+
+    public void Reset()
+    {
+        if (!hidden)
+        {
+            SetVisible(false);
+            StartCoroutine(DelayedStart());
+        }
+    }
+
+    private void SetVisible(bool value)
+    {
+        body.simulated = value;
+        sprite.enabled = value;
+    }
+
+    IEnumerator DelayedStart()
+    {
+        yield return new WaitForSeconds(resetDelay);
+
+        transform.position = start;
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        body.AddForce(initialForce);
+
+        SetVisible(true);
+    }
+}
---
+++ Ricochet/Assets/_Scripts/Goal_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Goal_Script : MonoBehaviour {
+    public Text Score;
+    private int _score = 0;
+
+    private void Start()
+    {
+        if (Score == null)
+        {
+            throw new System.Exception("No score text object assigned to goal");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ball")
+        {
+            Score.text = (++_score).ToString();
+            collision.GetComponent<Ball_Script>().Reset();
+        }
+    }
+}
