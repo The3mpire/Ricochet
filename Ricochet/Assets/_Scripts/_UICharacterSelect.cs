b33db5a10308670bc19c76eb4f3c61321671f433
Pinak Jalan <pinak.jalan@me.com>
2017-10-30 16:51:56 -0600

    Re- started character select because fucking sourcetree ate our shit

---
+++ Ricochet/Assets/_Scripts/_UICharacterSelect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _UICharacterSelect : MonoBehaviour {
+
+
+
+
+
+	private class Player
+	{
+		int team = 1;
+		string character = "bean";
+		int[] position = { 0, 0 };
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) 
+		{
+			//Debug.Log (Input.ke);
+		}
+		
+	}
+}
