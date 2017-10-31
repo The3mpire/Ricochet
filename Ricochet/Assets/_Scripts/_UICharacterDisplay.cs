d06c1f18b8759b3150bebbe1c6e3d7804b4a6447
Pinak Jalan <pinak.jalan@me.com>
2017-10-30 18:46:51 -0600

    Working on character select logic and updating the UI for different avatars and colors

---
+++ Ricochet/Assets/_Scripts/_UICharacterDisplay.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#region MonoBehaviour
+public class _UICharacterDisplay : MonoBehaviour {
+	#region Inspector Variables
+	[Header("Player Sprites")]
+	[Tooltip("Red Bean Sprite")]
+	[SerializeField]
+	private SpriteRenderer redBean1;
+	[Tooltip("Blue Bean Sprite")]
+	[SerializeField]
+	private SpriteRenderer blueBean1;
+	[Tooltip("Red Eggplant Sprite")]
+	[SerializeField]
+	private SpriteRenderer redEggplant1;
+	[Tooltip("Blue EggPlant Sprite")]
+	[SerializeField]
+	private SpriteRenderer blueEggplant1;
+
+	[Header("Cursor Sprites")]
+	[Tooltip("Bean Cursor")]
+	[SerializeField]
+	private SpriteRenderer beanCursor;
+	[Tooltip("Eggbplant Cursor")]
+	[SerializeField]
+	private SpriteRenderer eggplantCursor;
+	#region
+
+	public void UpdateCharacter(string newCharacter) {
+		
+	}
+	public void MoveCursor(string selection) {
+		
+	}
+	public void UpdateTeam(int team) {
+		
+	}
+	public void SetReady(bool state) {
+		
+	}
+}
+#endregion
