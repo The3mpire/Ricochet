4b32174ef1dd981862e4e854b7aec3934325e585
EKomperud <ericmkomperud@gmail.com>
2017-11-15 14:09:46 -0700

    Added basic KillZone script. Added KillZoneCollision to GameManager to accomdate. Added a KillZone prefab. Added the prefab to CatchNThrowPlayground scene to test.

---
+++ Ricochet/Assets/_Scripts/KillZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour {
+
+    private GameManager gameManagerInstance;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Collider2D hitCollider = col.collider;
+        if (gameManagerInstance != null || GameManager.TryGetInstance(out gameManagerInstance))
+        {
+            gameManagerInstance.KillZoneCollision(hitCollider.gameObject);
+        }
+    }
+}
