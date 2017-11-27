e1e4f059b340283dfaac2d4d55bba118cd34be17
Karla Kraiss <karkraiss@gmail.com>
2017-11-26 18:15:30 -0700

    Deleted a lot of the garbage scenes we didn't need Main menu now functions correctly (the mode select doesn't do anything)

---
+++ Ricochet/Assets/_Scripts/UI/MainMenuFunctions.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenuFunctions : MonoBehaviour {
+
+    #region Reference Variables
+    [Tooltip("The panel to be enabled when start is pressed")]
+    [SerializeField]
+    private GameObject optionsPanel;
+    [Tooltip("The first button to be selected when the panel is on")]
+    [SerializeField]
+    private GameObject optionsButton;
+    [Tooltip("The first button to be selected in the scene")]
+    [SerializeField]
+    private GameObject defaultButton;
+    #endregion
+
+    #region Hidden Variables
+    private GameManager gameManagerInstance;
+    #endregion
+
+    public void OpenGameOptions()
+    {
+        optionsPanel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(optionsButton);
+    }
+
+    public void CloseGameOptions()
+    {
+        optionsPanel.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(defaultButton);
+    }
+
+    public void SendGameMode()
+    {
+        if (gameManagerInstance != null || GameManager.TryGetInstance(out gameManagerInstance))
+        {
+
+        }
+    }
+}
