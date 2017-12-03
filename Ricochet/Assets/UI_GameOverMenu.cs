0cc351dd637a0b1e957e023cadbfa560a01cfc32
Christian <christian.50.roy@gmail.com>
2017-12-03 14:13:02 -0700

    Refactor Level Select

---
+++ Ricochet/Assets/UI_GameOverMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class UI_GameOverMenu : MonoBehaviour
+{
+    [SerializeField] private Button defaultSelectedButton;
+
+    #region MonoBehaviours
+    private void Start()
+    {
+        
+
+    }
+
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    public void ShowGameOverMenu()
+    {
+        EventSystem.current.SetSelectedGameObject(defaultSelectedButton);
+    }
+}
---
+++ Ricochet/Assets/_Scripts/LevelSelect.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class LevelSelect
+{
+    public const int MAIN_MENU = 0;
+    public const int CHARACTER_SELECT = 1;
+    public const int LEVEL_SELECT = 2;
+    public const int ART_SHOWCASE = 3;
+    public const int ELEVATOR = 4;
+    public const int TRAP = 5;
+    public const int DIAGONAL_ALLEY = 6;
+    public const int UP_N_OVER = 9;
+
+    public static List<int> glitchBallClassicLevels = new List<int>() {
+            ELEVATOR,
+            UP_N_OVER,
+            DIAGONAL_ALLEY
+        };
+
+    public static void LoadRandomLevel(List<int> levelSelection)
+    {
+        SceneManager.LoadSceneAsync(levelSelection[Random.Range(0, levelSelection.Count)]);
+    }
+
+    public static void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+
+    public static void LoadLevel(int buildIndex)
+    {
+        SceneManager.LoadSceneAsync(buildIndex);
+    }
+
+    public static void LoadCharacterSelect()
+    {
+        SceneManager.LoadSceneAsync(CHARACTER_SELECT);
+    }
+
+    public static void LoadLevelSelect()
+    {
+        SceneManager.LoadSceneAsync(LEVEL_SELECT);
+    }
+}
