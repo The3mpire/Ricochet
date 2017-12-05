87fd89114fadebda1ee5951f1c8856ca7ea092c8
Christian <christian.50.roy@gmail.com>
2017-12-05 15:39:38 -0700

    Start work, create script

---
+++ Ricochet/Assets/_Scripts/Player/PlayerTaunt.cs
@@ -0,0 +1,20 @@
+﻿using Rewired;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTaunt : MonoBehaviour
+{
+    private PlayerController playerController;
+    private Player rewiredPlayer;
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+
+    private void Start()
+    {
+        
+    }
+}
