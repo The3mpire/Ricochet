fb48aff433c857f9e5c0fc53418cfa8e8bbccd85
Pinak Jalan <pinak.jalan@me.com>
2018-01-15 19:35:59 -0700

    Added a few changes to arrows

---
+++ Ricochet/Assets/_Scripts/PointingArrows.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointingArrows : MonoBehaviour 
+{
+
+	#region Inspector Variables
+	[Tooltip("Target the arrow will follow")]
+	[SerializeField] public Transform target;
+	[Tooltip("How fast the arrow will move towards the goal")]
+	[SerializeField] public float strength = 100f;
+	[Tooltip("Minimum alpha value")]
+	[SerializeField] public float minimum = 0.0f;
+	[Tooltip("Maximum alpha value")]
+	[SerializeField] public float maximum = 1f;
+	[Tooltip("Duration of fade out for the arrow")]
+	[SerializeField] public float duration = 7.0f;
+	[Tooltip("Sprite to fade out")]
+	[SerializeField] public SpriteRenderer sprite;
+	#endregion
+
+	#region Private Variables
+	private float startTime;
+	#endregion
+
+	#region MonoBehaviour
+	void Start ()
+	{
+		startTime = Time.time;
+	}
+
+	void LateUpdate () 
+	{
+		Vector2 direction = target.position - transform.position;
+		float angle = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
+		Quaternion rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, strength * Time.deltaTime);
+		StartCoroutine (FadeOut ());
+	}
+	#endregion
+
+	#region Private Methods
+	private IEnumerator FadeOut()
+	{
+		float t = (Time.time - startTime) / duration;
+		sprite.color = new Color(1f,1f,1f,Mathf.SmoothStep(maximum, minimum, t)); 
+		yield return null;
+	}
+	#endregion
+}
