148c99b251e79bfdff82054b0c27107ca1ab069c
NA\ccarlson <doughtnerd@gmail.com>
2018-01-24 12:50:20 -0700

    Added camera effect shaders

---
+++ Ricochet/Assets/Shaders/VHSPostProcessEffect.shader
@@ -0,0 +1,52 @@
+﻿/*
+Base shader courtesy of https://github.com/staffantan/unity-vhsglitch 
+Customizations and efficiency changes made by Chris Carlson
+*/
+
+Shader "Hidden/VHSPostProcessEffect" {
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		_VHSTex ("VHS Glitch Texture", 2D) = "white" {}
+		_BleedColor ("Bleed Color", Color) = (0,0,0,0)
+		_BleedIntensity ("Inverse Scaling of bleed Intensity", float) = 15
+	}
+
+	SubShader {
+		Pass {
+			ZTest Always Cull Off ZWrite Off
+			Fog { Mode off }
+					
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+			#pragma fragmentoption ARB_precision_hint_fastest 
+			#include "UnityCG.cginc"
+
+			uniform sampler2D _MainTex;
+			uniform sampler2D _VHSTex;
+			float4 _BleedColor;
+			float _BleedIntensity;
+
+
+			fixed4 frag (v2f_img i) : COLOR{
+
+				fixed4 vhs = tex2D (_VHSTex, i.uv);
+				fixed4 c = tex2D (_MainTex, i.uv);
+
+				float4 bleed = tex2D(_MainTex, i.uv + float2(0.01, 0));
+				bleed += tex2D(_MainTex, i.uv + float2(0.02, 0));
+				bleed += tex2D(_MainTex, i.uv + float2(0.01, 0.01));
+				bleed += tex2D(_MainTex, i.uv + float2(0.02, 0.02));
+				bleed /= _BleedIntensity;
+
+				bleed *= _BleedColor;
+
+				vhs += bleed;
+
+				return c + vhs;
+			}
+			ENDCG
+		}
+	}
+Fallback off
+}
---
+++ Ricochet/Assets/_Scripts/Shaders/ChromaticAberrationEffect.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace CCShaders
+{
+    [DisallowMultipleComponent]
+    public sealed class ChromaticAberrationEffect : CameraShaderEffect
+    {
+
+        #region Inspector Variables
+
+        [SerializeField]
+        [Range(0, 1)]
+        [Tooltip("The intensity of the aberration (Distance of RGB channel split)")]
+        private float intensity = .025f;
+
+        [SerializeField]
+        [Range(0, 100)]
+        [Tooltip("Frequency of the aberration effect")]
+        private float frequency = 10f;
+
+        [SerializeField]
+        [Range(-5, 5)]
+        [Tooltip("Evolution speed of the effect")]
+        private float travelRate = 0f;
+
+        [SerializeField]
+        [Range(1, 100)]
+        [Tooltip("Frequency of the masking wave for the effect")]
+        private float maskFrequency = 10f;
+
+        [SerializeField]
+        [Range(-5, 5)]
+        [Tooltip("Evolution speed of the masking frequency")]
+        private float maskTravelRate = 2f;
+
+        [SerializeField]
+        [Range(1, 20)]
+        [Tooltip("How quickly the effect should simulate")]
+        private float simulationSpeed = 1f;
+
+        #endregion
+
+        #region Private Helpers
+
+        protected override void SetEffectValues(Material effectMaterial)
+        {
+            if (effectMaterial)
+            {
+                effectMaterial.SetFloat("_Intensity", this.intensity);
+                effectMaterial.SetFloat("_Frequency", this.frequency);
+                effectMaterial.SetFloat("_TravelRate", this.travelRate);
+                effectMaterial.SetFloat("_MaskFrequency", this.maskFrequency);
+                effectMaterial.SetFloat("_MaskTravelMultiplier", this.maskTravelRate);
+                effectMaterial.SetFloat("_SimulationSpeed", this.simulationSpeed);
+            }
+        }
+
+        #endregion
+    }
+}
