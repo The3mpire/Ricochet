792f57d31fa8bd4925b8ede4fcc70043af0f5286
Kyle Erickson <Kyle.Erickson@crengland.com>
2018-02-06 15:49:37 -0700

    Fix dash UI rotation speed.

--- Ricochet/Assets/FuelUIController.cs
+++ Ricochet/Assets/FuelUIController.cs
@@ -52,7 +52,7 @@ public class FuelUIController : MonoBehaviour
 
     public void Update()
     {
-        transform.Rotate(0f, 0f, rotateSpeed);
+        transform.Rotate(0f, 0f, rotateSpeed*Time.deltaTime);
 
         int count = 0;
 
