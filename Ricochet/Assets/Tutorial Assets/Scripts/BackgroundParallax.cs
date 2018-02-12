0915d7b9b9143b6abe4ff61021f243e73e698102
TROGDOR\dough <doughtnerd@gmail.com>
2018-02-11 17:31:15 -0700

    Completed parallax conversion

--- Ricochet/Assets/Tutorial Assets/Scripts/BackgroundParallax.cs
+++ Ricochet/Assets/Tutorial Assets/Scripts/BackgroundParallax.cs
@@ -3,10 +3,21 @@ using System.Collections;
 
 public class BackgroundParallax : MonoBehaviour
 {
-	public Transform[] backgrounds;				// Array of all the backgrounds to be parallaxed.
-	public float parallaxScale;					// The proportion of the camera's movement to move the backgrounds by.
-	public float parallaxReductionFactor;		// How much less each successive layer should parallax.
-	public float smoothing;						// How smooth the parallax effect should be.
+    [SerializeField]
+    [Tooltip("Array of all the backgrounds to be parallaxed.")]
+    private BackgroundItem[] backgrounds;				
+
+    [SerializeField]
+    [Tooltip("The proportion of the camera's movement to move the backgrounds by.")]
+    private float parallaxScale;					
+
+    [SerializeField]
+    [Tooltip("How much less each successive layer should parallax.")]
+    private float parallaxReductionFactor;		
+
+    [SerializeField]
+    [Tooltip("How smooth the parallax effect should be")]
+    private float smoothing;
 
 
 	private Transform cam;						// Shorter reference to the main camera's transform.
@@ -15,14 +26,12 @@ public class BackgroundParallax : MonoBehaviour
 
 	void Awake ()
 	{
-		// Setting up the reference shortcut.
 		cam = Camera.main.transform;
 	}
 
 
 	void Start ()
 	{
-		// The 'previous frame' had the current frame's camera position.
 		previousCamPos = cam.position;
 	}
 
@@ -35,17 +44,30 @@ public class BackgroundParallax : MonoBehaviour
 		// For each successive background...
 		for(int i = 0; i < backgrounds.Length; i++)
 		{
+            if (backgrounds[i].useCustomScale)
+            {
+                parallax = (previousCamPos.x - cam.position.x) * backgrounds[i].customScale;
+            }
+
 			// ... set a target x position which is their current position plus the parallax multiplied by the reduction.
-			float backgroundTargetPosX = backgrounds[i].position.x + parallax * (i * parallaxReductionFactor + 1);
+			float backgroundTargetPosX = backgrounds[i].background.position.x + parallax * (i * parallaxReductionFactor + 1);
 
 			// Create a target position which is the background's current position but with it's target x position.
-			Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+			Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].background.position.y, backgrounds[i].background.position.z);
 
 			// Lerp the background's position between itself and it's target position.
-			backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+			backgrounds[i].background.position = Vector3.Lerp(backgrounds[i].background.position, backgroundTargetPos, smoothing * Time.deltaTime);
 		}
 
 		// Set the previousCamPos to the camera's position at the end of this frame.
 		previousCamPos = cam.position;
 	}
+
+    [System.Serializable]
+    struct BackgroundItem
+    {
+        public Transform background;
+        public bool useCustomScale;
+        public float customScale;
+    }
 }
