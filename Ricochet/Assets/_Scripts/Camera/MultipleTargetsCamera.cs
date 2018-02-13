0c0227bf18183f91c33df3d1d9ce5091061f85c2
Cody Black <cody.black.47@gmail.com>
2018-02-12 17:56:14 -0700

    Altered camera movement script to zoom out when targets are spread too far horizontally across levels.

--- Ricochet/Assets/_Scripts/Camera/MultipleTargetsCamera.cs
+++ Ricochet/Assets/_Scripts/Camera/MultipleTargetsCamera.cs
@@ -10,17 +10,25 @@ public class MultipleTargetsCamera : MonoBehaviour
     [SerializeField] private float smoothTime = .5f;
     [SerializeField] private Vector2 minPos;
     [SerializeField] private Vector2 maxPos;
+    [SerializeField] private float maxZoomOut = 2f;
+    [SerializeField] private float zoomOutTime = 0.4f;
+    [SerializeField] private float zoomInTime = 0.7f;
 
+    private Camera camera;
     private GameManager manager;
     private Vector3 velocity;
+    private float zoomVel;
+    private float initialZoom;
 
     #region Monobehaviours
     public void Awake()
     {
+        camera = gameObject.GetComponent<Camera>();
         GameObject center = new GameObject("Center Transform");
         center.transform.parent = transform;
         center.transform.position = Vector3.zero;
         targets.Add(center.transform);
+        initialZoom = camera.orthographicSize;
     }
 
     public void Start()
@@ -50,23 +58,37 @@ public class MultipleTargetsCamera : MonoBehaviour
             return;
         }
 
-        Vector3 centerPoint = GetCenterPoint();
+        Bounds targetBounds = GetTargetBounds();
+        Vector3 centerPoint = targetBounds.center;
 
         float x = Mathf.Clamp(centerPoint.x, minPos.x, maxPos.x);
         float y = Mathf.Clamp(centerPoint.y, minPos.y, maxPos.y);
 
         Vector3 targetPoint = new Vector3(x, y) + offset;
-        
+
         transform.position = Vector3.SmoothDamp(transform.position, targetPoint, ref velocity, smoothTime);
+
+        if (targetBounds.extents.x > 2.25*maxPos.x)
+        {
+            float z = initialZoom + Mathf.Abs(targetBounds.extents.x - maxPos.x);
+            z = Mathf.Clamp(z, initialZoom, initialZoom + maxZoomOut);
+            float smoothZ = Mathf.SmoothDamp(camera.orthographicSize, z, ref zoomVel, zoomOutTime);
+            camera.orthographicSize = smoothZ;
+        }
+        else
+        {
+            float smoothZ = Mathf.SmoothDamp(camera.orthographicSize, initialZoom, ref zoomVel, zoomInTime);
+            camera.orthographicSize = smoothZ;
+        }
     }
     #endregion
 
     #region Getters
-    public Vector3 GetCenterPoint()
+    public Bounds GetTargetBounds()
     {
         if (targets.Count == 1)
         {
-            return targets[0].position;
+            return new Bounds(targets[0].position, targets[0].position);
         }
 
         Bounds bounds = new Bounds(targets[0].position, Vector3.zero);
@@ -78,7 +100,7 @@ public class MultipleTargetsCamera : MonoBehaviour
             }
         }
 
-        return bounds.center;
+        return bounds;
     }
     #endregion
 }
