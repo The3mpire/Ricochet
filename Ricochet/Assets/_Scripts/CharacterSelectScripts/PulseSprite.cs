2f693daeaa9ff44d84c06effc62b3c6e633ede0a
Christian <christian.50.roy@gmail.com>
2018-02-20 14:29:22 -0700

    Move script

---
+++ Ricochet/Assets/_Scripts/CharacterSelectScripts/PulseSprite.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PulseSprite : MonoBehaviour {
+
+    [SerializeField] private float growSize = 1.05f;
+    [SerializeField] private float shrinkSize = 0.95f;
+    [SerializeField] private float duration = 0.5f;
+
+    // Use this for initialization
+    void Start () {
+        Grow();
+	}
+
+    void Grow ()
+    {
+        transform.DOScale(growSize, duration).OnComplete(()=>Shrink());
+    }
+
+    void Shrink()
+    {
+        transform.DOScale(shrinkSize, duration).OnComplete(()=>Grow());
+    }
+}
