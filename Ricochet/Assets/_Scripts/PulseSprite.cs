3230f9c0a241ea34240fabf9d048973254921af3
Christian <christian.50.roy@gmail.com>
2018-02-20 13:10:45 -0700

    Refactor pulse sprite script

--- Ricochet/Assets/_Scripts/PulseSprite.cs
+++ Ricochet/Assets/_Scripts/PulseSprite.cs
@@ -5,20 +5,22 @@ using DG.Tweening;
 
 public class PulseSprite : MonoBehaviour {
 
-    
+    [SerializeField] private float growSize = 1.05f;
+    [SerializeField] private float shrinkSize = 0.95f;
+    [SerializeField] private float duration = 0.5f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         Grow();
 	}
 
     void Grow ()
     {
-        transform.DOScale(1.05f, .5f).OnComplete(()=>Shrink());
+        transform.DOScale(growSize, duration).OnComplete(()=>Shrink());
     }
 
     void Shrink()
     {
-        transform.DOScale(.95f, .5f).OnComplete(()=>Grow());
+        transform.DOScale(shrinkSize, duration).OnComplete(()=>Grow());
     }
 }
