65969430665e29f3b008498a804920e7ef6f20f9
Christian <christian.50.roy@gmail.com>
2018-02-20 12:57:32 -0700

    Add pulsing buttons

---
+++ Ricochet/Assets/_Scripts/PulseSprite.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PulseSprite : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+        Grow();
+	}
+
+    void Grow ()
+    {
+        transform.DOScale(1.05f, .5f).OnComplete(()=>Shrink());
+    }
+
+    void Shrink()
+    {
+        transform.DOScale(.95f, .5f).OnComplete(()=>Grow());
+    }
+}
