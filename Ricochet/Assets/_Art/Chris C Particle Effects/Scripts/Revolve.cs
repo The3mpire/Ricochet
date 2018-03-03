7cd975926024120307f04cfbd04319abe088df55
TROGDOR\dough <doughtnerd@gmail.com>
2018-03-02 17:49:14 -0700

    Working on Ice Powerup

---
+++ Ricochet/Assets/_Art/Chris C Particle Effects/Scripts/Revolve.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CCParticles
+{
+    [ExecuteInEditMode]
+    public class Revolve : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Transform center;
+
+        [SerializeField]
+        private float speed;
+
+        [SerializeField]
+        private Vector3 axis;
+
+        [SerializeField]
+        private Vector3 startPosition;
+
+
+        private void Start()
+        {
+            gameObject.transform.position = center.position + startPosition;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            gameObject.transform.RotateAround(this.center.position, this.axis, Time.deltaTime * speed);
+        }
+    }
+
+}
