0c809a4f4245ed16baea5f707b59e491c4657c3e
Cody Black <cody.black.47@gmail.com>
2018-03-11 18:17:52 -0600

    Starting to add a floating camera mode, which isn't going well.

--- Ricochet/Assets/_Scripts/Camera/MultipleTargetsCamera.cs
+++ Ricochet/Assets/_Scripts/Camera/MultipleTargetsCamera.cs
@@ -4,164 +4,185 @@ using UnityEngine;
 
 public class MultipleTargetsCamera : MonoBehaviour
 {
-    [Header("XY Movement Settings")]
-    [SerializeField] private List<Transform> targets;
-    [SerializeField] private Vector3 offset;
-    [SerializeField] private float smoothTime = .5f;
-    [SerializeField] private Vector2 minPos;
-    [SerializeField] private Vector2 maxPos;
+	[Header("XY Movement Settings")]
+	[SerializeField] private List<Transform> targets;
+	[SerializeField] private Vector3 offset;
+	[SerializeField] private float smoothTime = .5f;
+	[SerializeField] private Vector2 minPos;
+	[SerializeField] private Vector2 maxPos;
 
-    [Header("Zoom Out")]
-    [SerializeField] private float zoomOutTime;
-    [SerializeField] private float zoomInTime;
+	[Header("Zoom Out")]
+	[SerializeField] private float zoomOutTime;
+	[SerializeField] private float zoomInTime;
 
-    [Header("Level Wall Transforms")]
-    [SerializeField] private Transform topWallTransform;
-    [SerializeField] private Transform bottomWallTransform;
-    [SerializeField] private Transform leftWallTransform;
-    [SerializeField] private Transform rightWallTransform;
+	[Header("Manual Sizing Options")]
+	[SerializeField] private bool manualZooming;
+	[SerializeField] private float initialZoom = 0f;
+	[SerializeField] private float maxZoomOut;
+	[SerializeField] private float zoomRatio;
+	[SerializeField] private Vector2 zoomThresholds;
+
+	[Header("Level Wall Transforms")]
+	[Tooltip("Drag the top outer wall here")]
+	[SerializeField] private Transform topWallTransform;
+	[Tooltip("Drag the bottom outer wall here")]
+	[SerializeField] private Transform bottomWallTransform;
+	[Tooltip("Drag the left outer wall here")]
+	[SerializeField] private Transform leftWallTransform;
+	[Tooltip("Drag the right outer wall here")]
+	[SerializeField] private Transform rightWallTransform;
 
 
-    private Camera camera;
-    private GameManager manager;
-    private Vector3 velocity;
+	private Camera camera;
+	private GameManager manager;
+	private Vector3 velocity;
     
-    // zoom variables
-    private Transform[] outerWalls;
-    private Vector2 minZoomPos;
-    private Vector2 maxZoomPos;
-    private Vector2 zoomThresholds;
-    private float initialZoom;
-    private float maxZoomOut;
-    private float zoomRatio;
-    private float zoomVel;
-    private float aspectRatio;
+	// zoom variables
+	private Transform[] outerWalls;
+	private Vector2 minZoomPos;
+	private Vector2 maxZoomPos;
 
-    #region Monobehaviours
-    public void Awake()
-    {
-        camera = gameObject.GetComponent<Camera>();
-        aspectRatio = camera.aspect;
-        GameObject center = new GameObject("Center Transform");
-        center.transform.parent = transform;
-        center.transform.position = Vector3.zero;
-        targets.Add(center.transform);
+	private float zoomVel;
+	private float aspectRatio;
 
-        outerWalls = new Transform[] {topWallTransform, bottomWallTransform, leftWallTransform, rightWallTransform};
-        SetZoom(aspectRatio, outerWalls);
-        //initialZoom = camera.orthographicSize;
-        camera.orthographicSize = initialZoom;
-    }
+	#region Monobehaviours
 
-    public void Start()
-    {
-        if (!GameManager.TryGetInstance(out manager))
-        {
-            Debug.LogError("Camera unable to find GameManager", gameObject);
-            return;
-        }
+	public void Awake()
+	{
+		camera = gameObject.GetComponent<Camera>();
+		aspectRatio = camera.aspect;
+		GameObject center = new GameObject("Center Transform");
+		center.transform.parent = transform;
+		center.transform.position = Vector3.zero;
+		targets.Add(center.transform);
+
+		outerWalls = new Transform[] { topWallTransform, bottomWallTransform, leftWallTransform, rightWallTransform };
+		SetZoom(aspectRatio, outerWalls);
+		//initialZoom = camera.orthographicSize;
+		camera.orthographicSize = initialZoom;
+	}
+
+	public void Start()
+	{
+		if(!GameManager.TryGetInstance(out manager))
+		{
+			Debug.LogError("Camera unable to find GameManager", gameObject);
+			return;
+		}
         
-        List<GameObject> balls = manager.GetBallObjects();
+		List<GameObject> balls = manager.GetBallObjects();
 
-        foreach (PlayerController p in manager.GetPlayers())
-        {
-            targets.Add(p.transform);
-        }
-        foreach (GameObject go in balls)
-        {
-            targets.Add(go.transform);
-        }
-    }
+		foreach(PlayerController p in manager.GetPlayers())
+		{
+			targets.Add(p.transform);
+		}
+		foreach(GameObject go in balls)
+		{
+			targets.Add(go.transform);
+		}
+	}
 
-    public void LateUpdate()
-    {
-        if (targets.Count == 0)
-        {
-            return;
-        }
+	public void LateUpdate()
+	{
+		if(targets.Count == 0)
+		{
+			return;
+		}
 
-        Bounds targetBounds = GetTargetBounds();
-        Vector3 centerPoint = targetBounds.center;
+		Bounds targetBounds = GetTargetBounds();
+		Vector3 centerPoint = targetBounds.center;
 
-        float x = Mathf.Clamp(centerPoint.x, minZoomPos.x, maxZoomPos.x);
-        float y = Mathf.Clamp(centerPoint.y, minZoomPos.y, maxZoomPos.y);
+		float x = Mathf.Clamp(centerPoint.x, minZoomPos.x, maxZoomPos.x);
+		float y = Mathf.Clamp(centerPoint.y, minZoomPos.y, maxZoomPos.y);
 
-        Vector3 targetPoint = new Vector3(x, y) + offset;
+		Vector3 targetPoint = new Vector3(x, y) + offset;
 
-        transform.position = Vector3.SmoothDamp(transform.position, targetPoint, ref velocity, smoothTime);
+		transform.position = Vector3.SmoothDamp(transform.position, targetPoint, ref velocity, smoothTime);
 
-        if (targetBounds.extents.x > zoomThresholds.x)
-        {
-            float z = initialZoom + (zoomRatio / 100) * (targetBounds.extents.x - zoomThresholds.x);
-            z = Mathf.Clamp(z, initialZoom, initialZoom + maxZoomOut);
-            float smoothZ = Mathf.SmoothDamp(camera.orthographicSize, z, ref zoomVel, zoomOutTime);
-            camera.orthographicSize = smoothZ;
-        }
-        else if (targetBounds.extents.y > zoomThresholds.y)
-        {
-            float z = initialZoom + (zoomRatio / 100) * (targetBounds.extents.y - zoomThresholds.y);
-            z = Mathf.Clamp(z, initialZoom, initialZoom + maxZoomOut);
-            float smoothZ = Mathf.SmoothDamp(camera.orthographicSize, z, ref zoomVel, zoomOutTime);
-            camera.orthographicSize = smoothZ;
-        }
-        else
-        {
-            float smoothZ = Mathf.SmoothDamp(camera.orthographicSize, initialZoom, ref zoomVel, zoomInTime);
-            camera.orthographicSize = smoothZ;
-        }
+		if(targetBounds.extents.x > zoomThresholds.x)
+		{
+			float z = initialZoom + (zoomRatio / 100) * (targetBounds.extents.x - zoomThresholds.x);
+			z = Mathf.Clamp(z, initialZoom, initialZoom + maxZoomOut);
+			float smoothZ = Mathf.SmoothDamp(camera.orthographicSize, z, ref zoomVel, zoomOutTime);
+			camera.orthographicSize = smoothZ;
+		}
+		else if(targetBounds.extents.y > zoomThresholds.y)
+		{
+			float z = initialZoom + (zoomRatio / 100) * (targetBounds.extents.y - zoomThresholds.y);
+			z = Mathf.Clamp(z, initialZoom, initialZoom + maxZoomOut);
+			float smoothZ = Mathf.SmoothDamp(camera.orthographicSize, z, ref zoomVel, zoomOutTime);
+			camera.orthographicSize = smoothZ;
+		}
+		else
+		{
+			float smoothZ = Mathf.SmoothDamp(camera.orthographicSize, initialZoom, ref zoomVel, zoomInTime);
+			camera.orthographicSize = smoothZ;
+		}
 
-        minZoomPos = minPos - minPos * (camera.orthographicSize - initialZoom) / maxZoomOut;
-        maxZoomPos = maxPos - maxPos * (camera.orthographicSize - initialZoom) / maxZoomOut;
-    }
-    #endregion
+		minZoomPos = minPos - minPos * (camera.orthographicSize - initialZoom) / maxZoomOut;
+		maxZoomPos = maxPos - maxPos * (camera.orthographicSize - initialZoom) / maxZoomOut;
+	}
 
-    #region Getters
-    public Bounds GetTargetBounds()
-    {
-        if (targets.Count == 1)
-        {
-            return new Bounds(targets[0].position, targets[0].position);
-        }
+	#endregion
 
-        Bounds bounds = new Bounds(targets[0].position, Vector3.zero);
-        for (int i = 0; i < targets.Count; i++)
-        {
-            if (targets[i].gameObject.activeSelf)
-            {
-                bounds.Encapsulate(targets[i].position);
-            }
-        }
+	#region Getters
 
-        return bounds;
-    }
-    #endregion
+	public Bounds GetTargetBounds()
+	{
+		if(targets.Count == 1)
+		{
+			return new Bounds(targets[0].position, targets[0].position);
+		}
 
-    private void SetZoom(float aspectRatio, Transform[] outerWalls)
-    {
-        float hHalfDistance = Mathf.Abs(outerWalls[3].localPosition.x - outerWalls[2].localPosition.x) / 2;
-        float vHalfDistance = Mathf.Abs(outerWalls[1].localPosition.y - outerWalls[0].localPosition.y) / 2;
-        float sceneRatio = hHalfDistance / vHalfDistance;
-        if (sceneRatio > aspectRatio)
-        {
-            initialZoom = vHalfDistance;
-			maxZoomOut = hHalfDistance / aspectRatio - initialZoom;
-            zoomThresholds[0] = initialZoom / 1.1f * aspectRatio - 3 ;
-            zoomThresholds[1] = vHalfDistance;
-            maxPos[0] = Mathf.Abs(hHalfDistance - initialZoom * aspectRatio);
-            minPos[0] = -maxPos[0];
-            zoomRatio = maxPos[0] / maxZoomOut * 100;
-        }
-        else
-        {
-            initialZoom = hHalfDistance / aspectRatio;
-            maxZoomOut = (vHalfDistance / aspectRatio - initialZoom) / 2;
-            zoomThresholds[0] = hHalfDistance;
-            zoomThresholds[1] = initialZoom / 1.1f - 3;
-            maxPos[1] = Mathf.Abs(vHalfDistance - initialZoom);
-            minPos[1] = -maxPos[1];
-            zoomRatio = maxPos[1] / maxZoomOut * 100;
-        }
-        minZoomPos = minPos;
-        maxZoomPos = maxPos;
-    }
+		Bounds bounds = new Bounds(targets[0].position, Vector3.zero);
+		for(int i = 0; i < targets.Count; i++)
+		{
+			if(targets[i].gameObject.activeSelf)
+			{
+				bounds.Encapsulate(targets[i].position);
+			}
+		}
+
+		return bounds;
+	}
+
+	#endregion
+
+	private void SetZoom(float aspectRatio, Transform[] outerWalls)
+	{
+		float hHalfDistance = Mathf.Abs(outerWalls[3].localPosition.x - outerWalls[2].localPosition.x) / 2;
+		float vHalfDistance = Mathf.Abs(outerWalls[1].localPosition.y - outerWalls[0].localPosition.y) / 2;
+		float sceneRatio = hHalfDistance / vHalfDistance;
+
+		// automatically set zoom based on level dimensions
+		if(!manualZooming)
+		{
+			if(sceneRatio > aspectRatio)
+			{
+				Debug.Log("Horizontal Level");
+				initialZoom = vHalfDistance;
+				maxZoomOut = hHalfDistance / aspectRatio - initialZoom;
+				zoomThresholds[0] = initialZoom / 1.1f * aspectRatio - 3;
+				zoomThresholds[1] = initialZoom;
+				maxPos[0] = Mathf.Abs(hHalfDistance - initialZoom * aspectRatio);
+				minPos[0] = -maxPos[0];
+				zoomRatio = maxPos[0] / maxZoomOut * 100;
+			}
+			else
+			{
+				Debug.Log("Vertical Level");
+				initialZoom = hHalfDistance / aspectRatio;
+				maxZoomOut = vHalfDistance - initialZoom;
+				zoomThresholds[0] = initialZoom * aspectRatio;
+				zoomThresholds[1] = initialZoom / 1.1f - 3;
+				maxPos[1] = Mathf.Abs(vHalfDistance - initialZoom);
+				minPos[1] = -maxPos[1];
+				// set other dimension if not autozoom
+				zoomRatio = maxPos[1] / maxZoomOut * 100;
+			}
+		}
+
+		minZoomPos = minPos;
+		maxZoomPos = maxPos;
+	}
 }
