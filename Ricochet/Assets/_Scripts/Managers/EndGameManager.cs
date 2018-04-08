23d853ace8e4be87d18c53358f258cec74f8e5e5
Kyle Erickson <Kyle.Erickson@crengland.com>
2018-04-07 23:04:19 -0600

    Started updating End Game layout.

--- Ricochet/Assets/_Scripts/Managers/EndGameManager.cs
+++ Ricochet/Assets/_Scripts/Managers/EndGameManager.cs
@@ -1,27 +1,96 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using DG.Tweening;
+using Enumerables;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class EndGameManager : MonoBehaviour
 {
-    [Tooltip("Drag the UI's EndGameText here")]
-    [SerializeField] private Text EndGameText;
-    [SerializeField] private GameObject defaultSelectedButton;
+    [Header("Game Stats")]
     [SerializeField] private GameDataSO gameData;
+    [Tooltip("Drag the winning team text here")]
+    [SerializeField] private Text _winTeamText;
+    [Tooltip("Drag the winning message here")]
+    [SerializeField] private Text _winMessageText;
+    [SerializeField] private Transform _winMessageEnd;
+    [Tooltip("Drag the winning team text here")]
+    [SerializeField] private Text _redScoreText;
+    [Tooltip("Drag the winning team text here")]
+    [SerializeField] private Text _blueScoreText;
+
+    [Header("Characters")]
+    [SerializeField]
+    [Tooltip("0 - Cop, 1 - Cat, 2 - Sushi, 3 - Computer")]
+    private GameObject[] _panel1Characters = new GameObject[4];
+    [SerializeField]
+    [Tooltip("0 - Cop, 1 - Cat, 2 - Sushi, 3 - Computer")]
+    private GameObject[] _panel2Characters = new GameObject[4];
+    [SerializeField]
+    [Tooltip("Position to move Character Panel 1 to.")]
+    private Transform _charPanel1to;
+    [SerializeField]
+    [Tooltip("Position to move Character Panel 1 to.")]
+    private Transform _charPanel2to;
+
+    [Header("Menu")]
+    [SerializeField] private GameObject _menuPanel;
+    [SerializeField] private GameObject defaultSelectedButton;
+
+    private Transform _charPanel1;
+    private Transform _charPanel2;
+    private Sequence _winTeamSequence;
+    private Sequence _winMessageSequence;
 
     #region MonoBehaviours
     private void Start()
     {
-        ShowGameOverMenu();
-        EndGameText.text = (gameData.GetGameWinner() == Enumerables.ETeam.None) ? "Tie Game!" : "Congratulations " + gameData.GetGameWinner().ToString() + "!"; 
+        _charPanel1 = _panel1Characters[0].transform.parent.transform;
+        _charPanel2 = _panel2Characters[0].transform.parent.transform;
+        SetUpSequences();
+        StartCoroutine(ShowGameWinner());
+        ShowMenu();
+        
     }
     #endregion
 
-    public void ShowGameOverMenu()
+    #region Coroutines
+
+    IEnumerator ShowGameWinner()
+    {
+        _winTeamSequence.Play();
+        _winMessageSequence.Play();
+        yield return _winTeamSequence.WaitForCompletion();
+        _charPanel1.DOMove(_charPanel1to.position, 2f);
+        _charPanel2.DOMove(_charPanel2to.position, 2f);
+    }
+
+    #endregion
+
+    #region Private Functions
+
+    private void SetUpSequences()
+    {
+        _winTeamSequence = DOTween.Sequence();
+        _winTeamSequence.Append(_winTeamText.transform.DOPunchScale(Vector3.one, 2f, 2, 0f));
+        _winTeamSequence.Pause();
+
+        _winMessageSequence = DOTween.Sequence();
+        _winMessageSequence.SetDelay(1f);
+        _winMessageSequence.Append(_winMessageText.transform.DOMove(_winMessageEnd.position, 1f));
+        _winMessageSequence.Append(_winMessageText.transform.DOPunchPosition(_winMessageText.transform.right*10, 2f, 10, 0));
+        _winMessageSequence.Pause();
+    }
+
+    #endregion
+
+
+    public void ShowMenu()
     {
         EventSystem.current.SetSelectedGameObject(defaultSelectedButton);
     }
 
+    #region Menu Functions
     public void PlayAgain()
     {
         gameData.ResetGameStatistics();
@@ -31,7 +100,6 @@ public class EndGameManager : MonoBehaviour
     public void PlayRandomLevel()
     {
         gameData.ResetGameStatistics();
-        // TODO: Switch here on GameData game type to load from correct playlist
         LevelSelect.LoadRandomLevel(LevelSelect.glitchBallClassicLevels);
     }
 
@@ -52,4 +120,5 @@ public class EndGameManager : MonoBehaviour
         gameData.ResetGameStatistics();
         LevelSelect.LoadMainMenu();
     }
+    #endregion
 }
